828e296fabe844c4554009b32eb566217ff79307
Matthew Vaccaro <home@Matthews-MBP-4.hsd1.tn.comcast.net>
2019-11-15 19:02:41 -0600

    small changes

--- src/components/forms/AddImmunization.js
+++ src/components/forms/AddImmunization.js
@@ -1,36 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 import { withFormik, Form, Field } from "formik";
-import axios from "axios";
 import * as yup from "yup";
+import api from "../utils/api.js";
 
-const AddImmunization = ({ touched, errors }) => {
+const AddImmunization = ({ touched, errors, isSubmitting }) => {
   return (
     <div>
       <Form>
-        <label htmlFor="date"> Date Administered</label>
-        <Field type="text" name="date" placeholder="Example: 04/05/1995" />
-        {touched.date && errors.date && <p>Please Add Date</p>}
+        <div>
+          <label htmlFor="date"> Date Administered</label>
+          <Field type="text" name="date" placeholder="Example: 04/05/1995" />
+          {touched.date && errors.date && <p>Please Add Date</p>}
+        </div>
 
-        <label htmlFor="immunization"> Immunization and Dose</label>
-        <Field
-          type="text"
-          name="immunization"
-          placeholder="Example: Example: Hepatitis B  (2nd dose)"
-        />
-        {touched.immunization && errors.immunization && (
-          <p>Please Add Immunization</p>
-        )}
+        <div>
+          <label htmlFor="immunization"> Immunization and Dose</label>
+          <Field
+            type="text"
+            name="immunization"
+            placeholder="Example: Example: Hepatitis B  (2nd dose)"
+          />
+          {touched.immunization && errors.immunization && (
+            <p>Please Add Date</p>
+          )}
+        </div>
 
-        <label htmlFor="location"> Administered Location</label>
-        <Field
-          type="text"
-          name="location"
-          placeholder="Example: Christis Medical - CA"
-        />
-        {touched.location && errors.location && <p>Please Add Location</p>}
+        <div>
+          <label htmlFor="location"> Administered Location</label>
+          <Field
+            type="text"
+            name="location"
+            placeholder="Example: Christis Medical - CA"
+          />
+          {touched.location && errors.location && <p>Please Add Location</p>}
+        </div>
 
-        <button type="submit"> Add </button>
+        <button disabled={isSubmitting} type="submit">
+          {" "}
+          Add{" "}
+        </button>
       </Form>
     </div>
   );
@@ -50,12 +59,16 @@ export default withFormik({
     location: yup.string().required()
   }),
   //Event Handler needs an endpoint to .post() to & a completed .then()
-  handleSubmit: values => {
-    axios
-      .post("", values)
-      .then(res => {})
+  handleSubmit: (values, { setSubmitting, resetForm }) => {
+    api()
+      .post("/record/addImmunization", values)
+      .then(res => {
+        console.log("it worked:", res.data);
+      })
       .catch(err => {
         console.log("error:", err);
+        setSubmitting(false);
+        resetForm();
       });
   }
 })(AddImmunization);
