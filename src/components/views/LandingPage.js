c736e0b4651eb8d934ab825b3c4ff5093b389949
Michelle Scott <scottmm374@gmail.com>
2019-11-20 17:07:03 -0500

    Added style to buttons on landing page

--- src/components/views/LandingPage.js
+++ src/components/views/LandingPage.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "../utils/styledComponents.js";
 
 function LandingPage() {
   return (
     <div>
       <h1>Choose your role.</h1>
       <Link to="/patient-login">
-        <button>Patient</button>
+        <Button>Patient</Button>
       </Link>
 
       <Link to="/med-login">
-        <button>Provider</button>
+        <Button>Provider</Button>
       </Link>
     </div>
   );
