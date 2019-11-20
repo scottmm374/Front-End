a9c40264da2768ddbb2b6000d64b9b6a771a9138
Michelle Scott <scottmm374@gmail.com>
2019-11-20 14:19:18 -0500

    grant permission working

--- src/components/forms/PermissionForm.js
+++ src/components/forms/PermissionForm.js
@@ -4,35 +4,71 @@ import React, { useState, useEffect } from "react";
 import api from "../utils/api";
 
 const PermissionForm = props => {
-  const [patient, setPatient] = useState();
+  // const [updatePermission, setUpdatePermission] = useState({
+  //   permission: true,
+  //   patientId: 0,
+  //   medproId: 0
+  // });
+
   const [permission, setPermission] = useState({
-    permission: false,
-    patientId: 0,
-    medproId: 0
+    permission: true,
+    patientId: null,
+    medproId: null
   });
 
-  const id = props.params.match.id;
+  // const id = props.params.match.id;
   //   console.log("token", getParentId());
 
-  useEffect(() => {
+  const handlePermission = e => {
+    e.preventDefault();
     api()
-      .get(`/user/${id}`) // put {props.match.params.id} when we have things linked up.
+      .post("perm/add", permission)
       .then(res => {
-        setPatient(res.data);
+        console.log("perm", res.data);
       })
       .catch(err => {
-        console.log("patient-child", err);
+        console.log("perm", err);
       });
-  }, []);
+  };
+
+  const handleChange = e => {
+    setPermission({ ...permission, [e.target.name]: e.target.value });
+  };
 
   //   console.log("permission", res.data);
-  console.log("PatientState", patient);
+  console.log("permission", permission);
+  // console.log("patient", updatePermission);
 
   return (
     <div>
-      <form>
-        <input type="text" name="medproId" value="medproId" />
-        <input type="text" name="patientId" value={id} />
+      <h1>Permission</h1>
+      <form onSubmit={handlePermission}>
+        <input
+          type="text"
+          name="medproId"
+          placeholder="Med Id"
+          value={permission.medproId}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="patientId"
+          placeholder="Child id"
+          value={permission.id}
+          onChange={handleChange}
+        />
+        {/* <label>
+          
+          <input
+            type="radio"
+            name="permission"
+            value="true"
+            checked={true}
+            onChange={handleChange}
+          />
+        </label> */}
+
+        <button>Give Permission</button>
       </form>
     </div>
   );
