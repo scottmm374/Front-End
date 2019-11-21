c65ebc860f3ab3fca3ab7720efe18a0b3ab1fae8
Michelle Scott <scottmm374@gmail.com>
2019-11-20 22:20:59 -0500

    Created dropdown for update child

--- src/components/ChildCard.js
+++ src/components/ChildCard.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const ChildCard = (props) => {
-    //console.log(child, doctor);
-    
-    return (
-        <div>
-            <Link 
-                to={{
-                    pathname:`/child-account/${props.child.firstName}${props.child.lastName}`,
-                    state: {
-                        child: props.child,
-                        shots: props.shots
-                    }
-            }}>{props.child.firstName} {props.child.lastName}</Link>
-        </div>
-    );
+const ChildCard = props => {
+  //console.log(child, doctor);
+
+  return (
+    <div>
+      <Link
+        to={{
+          // pathname:`/child-account/${props.child.firstName}${props.child.lastName}`,
+          pathname: `/child-account/${props.child.id}`,
+          state: {
+            child: props.child,
+            shots: props.shots
+          }
+        }}
+      >
+        {props.child.firstName} {props.child.lastName}
+      </Link>
+    </div>
+  );
 };
 
 export default ChildCard;
